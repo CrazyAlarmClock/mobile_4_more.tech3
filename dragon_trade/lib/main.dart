3984efa874aeb1c7fcbf779edc124fccdffdc56b
Pavel Malyshev <CrazyAlarmClock@gmail.com>
2021-10-08 21:10:28 +0300

    home add

--- dragon_trade/lib/main.dart
+++ dragon_trade/lib/main.dart
@@ -1,28 +1,34 @@
+import 'package:dragon_trade/screens/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class Login extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _LoginState createState() => _LoginState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
---
+++ dragon_trade/lib/screens/home.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.blue,
+        title: Text(
+          "TEXT",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: SlidingUpPanel(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(24.0), topRight: Radius.circular(24.0)),
+        minHeight: 300,
+        maxHeight: 600,
+        panel: SingleChildScrollView(
+          child: Column(
+            children: [Text('kek')],
+          ),
+        ),
+        body: Center(
+          child: Container(
+              padding: EdgeInsets.symmetric(vertical: 50),
+              alignment: Alignment.topCenter,
+              child: Text("DRAGON")),
+        ),
+      ),
+    );
+  }
+}
