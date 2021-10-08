eb0168a58ef953787b45b44a9d8925318daf15ff
Pavel Malyshev <CrazyAlarmClock@gmail.com>
2021-10-08 23:41:40 +0300

    colors + text

---
+++ dragon_trade/lib/theme/theme_settings.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static Color  primary = Color(0xff3A83F1);
+  static TextStyle text = TextStyle(color: Color(0xff333333), fontSize: 16, fontWeight: FontWeight.w500);
+  static TextStyle bold = TextStyle(color: Color(0xff333333), fontSize: 16, fontWeight: FontWeight.w900);
+}
