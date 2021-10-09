1f54ec3e764ae7b43d20b637606bb7411d8a1392
fatkullinalina <48031857+fatkullinalina@users.noreply.github.com>
2021-10-10 01:00:01 +0300

    balance+padding

--- dragon_trade/lib/screens/chapters/chapter_0.dart
+++ dragon_trade/lib/screens/chapters/chapter_0.dart
@@ -11,7 +11,6 @@ List<ChapterModel> chapterZero = [
       buttons: [
         BottomButton(
           name: 'Далее',
-         
         )
       ],
       isBackButton: false,
@@ -22,28 +21,27 @@ List<ChapterModel> chapterZero = [
             height: 48,
           ),
           FadeWidget(
-              widget: RichText(
-                  text: TextSpan(
-            text:
-                '  Однажды, в одной далёкой-далёкой стране родился дракон. И разумеется как и все драконы он с самого детства очень любил золото. Но к сожалению, в наследство от матушки и папеньки ему досталось не так уж и много... ',
-                
-            style: AppColors.text,
-
-            children: [
-              TextSpan(
-                  text: 'Всего тысяча золотых монет.',
-                  style:AppColors.bold)
-            ],
-          )))
+              widget: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: RichText(
+                text: TextSpan(
+              text:
+                  '  Однажды, в одной далёкой-далёкой стране родился дракон. И разумеется как и все драконы он с самого детства очень любил золото. Но к сожалению, в наследство от матушки и папеньки ему досталось не так уж и много... ',
+              style: AppColors.text,
+              children: [
+                TextSpan(
+                    text: 'Всего тысяча золотых монет.', style: AppColors.bold)
+              ],
+            )),
+          ))
         ],
       )),
-      ChapterModel(
-        isBackButton: false,
+  ChapterModel(
+      isBackButton: false,
       name: 'Глава 0. Рождение дракона',
       buttons: [
         BottomButton(
           name: 'Далее',
-           
         )
       ],
       text: Column(
@@ -53,21 +51,28 @@ List<ChapterModel> chapterZero = [
             height: 48,
           ),
           FadeWidget(
-              widget: Text(
-                '  И стал дракон думы думать. Как же скопить ему золота? Как распорядться своими богатствами? Но проезжал на его удачу в тех краях странник. И пожалел он молодого дракона — согласился помочь ему.',
+              widget: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+            '  И стал дракон думы думать. Как же скопить ему золота? Как распорядться своими богатствами? Но проезжал на его удачу в тех краях странник. И пожалел он молодого дракона — согласился помочь ему.',
             style: AppColors.text,
-            
-          )),
-             SizedBox(
+          ),
+              )),
+          SizedBox(
             height: 32,
           ),
           FadeWidget(
-              widget: Text(
-                'Так началась славная история Леонида и дракона…',
+              widget: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+            'Так началась славная история Леонида и дракона…',
             textAlign: TextAlign.center,
-            style:TextStyle(color: Color(0xff333333),fontSize: 20,fontWeight: FontWeight.w900),
-            
-          ))
+            style: TextStyle(
+                  color: Color(0xff333333),
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900),
+          ),
+              ))
         ],
       ))
 ];
